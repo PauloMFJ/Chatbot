4fa23e4becc7b31593d3e7bb1ff3db4f582c078f
Paulo Jorge <mfjpaulo@gmail.com>
2020-12-29 19:53:59 +0000

    heroku tweaks

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
+import os
+
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
+    THREADED = True
+    PORT = int(os.environ.get('PORT', 5000))
     # This shouldn't be visible
     SECRET_KEY = '\xe3\xa3\xecK>\x82\xc1\xdfH=\xd0S\xb3\x9eX\x97\xfd\xeb\xefO\xdf\xda\x10\x96'
 
